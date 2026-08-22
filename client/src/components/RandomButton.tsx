ee2c30760b7158629c41969294a0906927517090
Crynner <calvin.shih19@gmail.com>
2026-08-22 16:43:02 +1200

    adjusted randombutton styling and nearby for placement

--- client/src/components/RandomButton.tsx
+++ client/src/components/RandomButton.tsx
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import { apiFetch } from "../api"
 import type { Movie } from "../types/movie";
+import "../styles/RandomButton.css"
 
 
 type RandomButtonProps = {
@@ -8,7 +9,7 @@ type RandomButtonProps = {
 }
 
 export default function RandomButton({onMovieGet} : RandomButtonProps) {
-    const [buttonName, setButtonName] = useState("Go to Random Movie");
+    const [buttonName, setButtonName] = useState("Want a random movie?");
     const getRandom = async () => {
         try {
             setButtonName("Generating...")
@@ -18,12 +19,12 @@ export default function RandomButton({onMovieGet} : RandomButtonProps) {
             setButtonName("Generated!")
         } catch (err: unknown) {
             console.log(err instanceof Error ? err.message : "Something went wrong with RandomButton.");
-            setButtonName("Go to Random Movie")
+            setButtonName("Want a random movie?")
         }
     }
 
     return (
-        <button type="button" onClick={getRandom}>
+        <button type="button" onClick={getRandom} className="cursor-pointer text-sm" id="random-button">
             {buttonName}
         </button>
     )
